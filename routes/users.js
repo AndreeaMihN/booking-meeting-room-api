b4584a425f705448dd5734ca07008c342a076347
Andreea Mihalcea <SESA719763@se.com>
2024-02-24 00:56:28 +0200

    create user

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,73 @@
 const { User } = require('../models/user');
 const express = require('express');
+const bcryptjs = require('bcryptjs');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const userList = await User.find();
-  if (!roomList) {
+  const userList = await User.find().select('-passwordHash');
+  if (!userList) {
     res.status(500).json({ success: false });
   }
   res.send(userList);
 });
 
-router.post(`/`, (req, res) => {
-  const user = new User({
-    name: req.body.name,
-  });
-  user
-    .save()
-    .then((createdUser) => {
-      res.status(201).json(createdUser);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id).select('-passwordHash');
+  if (!user) {
+    return res.status(500).json({ message: 'The user with the given ID was not found.' });
+  }
+  res.status(200).send(user);
+});
+
+router.post(`/`, async (req, res) => {
+  let user = new User({
+    name: req.body.name, 
+    email: req.body.email, 
+    passwordHash: bcryptjs.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  })
+  user = await user.save();
+  if(!user)
+  return res.status(500).send('The user cannot be created!');
+
+  return res.send(user);
+});
+
+router.put('/:id', async (req, res) => {
+  const userExist = await User.findById(req.params.id);
+  let newPassword;
+  if(req.body.password){
+    newPassword = bcryptjs.hashSync(req.body.password, 10)
+
+  } else {
+    newPassword= userExist.passwordHash
+  }
+  const user = await User.findByIdAndUpdate(
+      req.params.id, 
+      {
+        name: req.body.name, 
+        email: req.body.email, 
+        passwordHash: newPassword,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+      },
+      {
+          new: true
       });
-    });
+
+  if(!user) return res.status(500).send('The user cannot be updated');
+  return res.send(user);
 });
 
 module.exports = router;
