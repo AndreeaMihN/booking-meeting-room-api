14b6adf2044b2363de413ad3665f88069ea28569
Andreea Mihalcea <SESA719763@se.com>
2024-02-24 12:08:35 +0200

    exclude routes from authentication

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,10 +2,21 @@ var { expressjwt: jwt } = require("express-jwt");
 
 function authJwt() {
     const secret = process.env.secret;
+    const api = process.env.API_URL
     return jwt({
         secret,
         algorithms: ['HS256']
-    });
+    }).unless({
+        path: [
+            {
+                url: /\/api\/v1\/rooms(.*)/,
+                methods: ['GET', 'OPTIONS'],
+            },
+            `${api}/users/login`,
+            `${api}/users/register`,
+            // { url: /(.*)/ },
+        ],
+    })
 }
 
 module.exports = authJwt;
